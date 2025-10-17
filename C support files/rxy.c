982b20e8d948859eb7ffca3383c04518352851e0
harun2301 <oscar_harun@comunidad.unam.mx>
2025-10-17 12:57:55 -0600

    finish x_corr and auto_x_corr

--- C support files/rxy.c
+++ C support files/rxy.c
@@ -10,34 +10,128 @@
  * gcc [name].c -lm -o [name].out
  * 
  * Run command on linux terminal
- * ./[nombre].out
-*/
+ * ./[nombre].out//
+//*/
 
 #include <stdio.h>
 
-#define N 	7
+/* Uncomment for different tests */
+//#define N 	5	// length of sequence x 
+//#define M 	4	// length of sequence y
+#define N 	7	// length of sequence x 
+#define M 	7	// length of sequence y
+//#define N 	4	// length of sequence x 
+//#define M 	3	// length of sequence y
+//#define N 	5	// length of sequence x 
+//#define M 	7	// length of sequence y
 
+#define	rxy_length	N+M-1	// length of sequence rxy
+#define	rxx_length	2*N-1	// length of sequence rxx    
+
+/* Uncomment for different tests */
+//float x[N] = {2,2,2,2,2};
+//float y[M] = {2,2,2,2};
 float x[N] = {0,1,2,3,2,1,0};
-float y[N] = {0,1,2,3,2,1,0};
-float rxy[N];
-float *ap_x, *apy;
+float y[M] = {0,1,2,3,2,1,0};
+//float x[N] = {3,3,3,3};
+//float y[M] = {3,3,3};
+//float x[N] = {1,1,1,1,1};
+//float y[M] = {1,1,1,1,1,1,1};
 
+float rxy[rxy_length];
+float rxx[rxx_length];
+float *ap_x, *ap_y, *ap_r;
 int n, i, j;
 
 void cross_r_xy(void);
+void auto_cross_r_xy(void);
+
 
 int main(void){
 
+	cross_r_xy();
+
+	for(i=0;i<rxy_length;i++)
+		printf("%.2f\n", rxy[i]);
+
+	printf("------------------------\n");
+
+	auto_cross_r_xy();
+	
+	for(i=0;i<rxy_length;i++)
+		printf("%.2f\n", rxx[i]);
+	
 	return 0;
 }
 
 void cross_r_xy(){
-	int cont = 0;
-	int desp = 0;
+	ap_r = &rxy[0];
 
-	for(i=0; i<N; i++){
+	// for l<=0
+	int cont = 1;
+	ap_x = &x[0];
+	ap_y = &y[M-1];
+
+	for(i=0; i<M; i++){
+
 		for(n=0; n<cont; n++){
-			//E += signal[n] * signal[n];
+			// movable sequence is y
+			*ap_r += *(ap_x+n) * *(ap_y+n);
 		}
+		ap_r++;
+		ap_y--;
+		cont++;
 	}
+	
+	// for l>0
+	cont = N-1;
+	ap_x = &x[1];
+	ap_y = &y[0];
+	
+	for(i=0; i<N-1; i++){
+		for(n=0; n<cont; n++){
+			// movable sequence is x
+			*ap_r += *(ap_x+n) * *(ap_y+n);
+		}
+		ap_r++;
+		ap_x++;
+		cont--;
+	}	
+
+}
+
+void auto_cross_r_xy(void){
+	ap_r = &rxx[0];
+
+	// for l<=0
+	int cont = 1;
+	ap_x = &x[0];
+	ap_y = &x[M-1];
+
+	for(i=0; i<M; i++){
+
+		for(n=0; n<cont; n++){
+			// movable sequence is y
+			*ap_r += *(ap_x+n) * *(ap_y+n);
+		}
+		ap_r++;
+		ap_y--;
+		cont++;
+	}
+	
+	// for l>0
+	cont = N-1;
+	ap_x = &x[1];
+	ap_y = &x[0];
+	
+	for(i=0; i<N-1; i++){
+		for(n=0; n<cont; n++){
+			// movable sequence is x
+			*ap_r += *(ap_x+n) * *(ap_y+n);
+		}
+		ap_r++;
+		ap_x++;
+		cont--;
+	}	
+
 }
